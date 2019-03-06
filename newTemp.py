523d69ca3584c540b88c3141a924a31453802b64
Fanming <46982832+FanmingSun@users.noreply.github.com>
2019-03-06 15:26:30 +0800

    Create newTemp.py

---
+++ newTemp.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#%%
+  j=0
+  for item in ['bear','bird','mouse','ant']:
+      print(j,item)
+      j+=1
+
+
+#%%
+
+  for counter,item  in enumerate (['bear','bird','mouse','ant']):
+      print(counter,item)
+      
+     
+        
+      
+#%%
+  j=0
+  for item in ['bear','bird','mouse','ant','elephant']:
+      print(j,item)
+      j+=1
